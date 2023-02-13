5b3907e7e464295468592acbe8670707b41f56a8
WolfGang <95577533+WolfGang198@users.noreply.github.com>
2023-02-13 15:45:52 -0300

    Add files via upload

---
+++ atv4/src/atv4/Atv4.java
@@ -0,0 +1,19 @@
+package atv4;
+
+import java.util.Scanner;
+
+public class Atv4 {
+
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        System.out.printf("Escreva um valor em dolar: U$");
+        double n1 = num.nextInt();
+        double r = n1 * 5.10;
+        System.out.printf("Este valor quivale a: R$" +r);
+        
+        
+               
+        
+    }
+    
+}
