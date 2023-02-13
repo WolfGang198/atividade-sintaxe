8b10ce029792e31b6bbf4fc74aa998a411597e01
WolfGang <95577533+WolfGang198@users.noreply.github.com>
2023-02-13 13:57:26 -0300

    Add files via upload

--- atv2/src/atv2/Calc.java
+++ atv2/src/atv2/Calc.java
@@ -2,9 +2,6 @@ package atv2;
 
 public class Calc {
     public int numero;
-    public int a1 = 0;
-    public int a2;
-    public int soma;
     
     public void soma(){
         System.out.print(this.numero);
@@ -13,10 +10,6 @@ public class Calc {
         System.out.print(numero);
         System.out.println("");
     }
-    
-    //public void tot(){
-    //--    System.out.println(soma);
-    //}
     
     public int getNumero(){
     return numero;
